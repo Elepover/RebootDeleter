7e202a8261ff3499c9a7cc24d355c4282d54edf7
Elepover <elepover@outlook.com>
2020-02-12 17:07:43 +0800

    添加项目文件。

---
+++ FormMain.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace RebootDeleter {
+    partial class FormMain {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(152, 10);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormMain";
+            this.Text = "RebootDeleter";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RebootDeleter {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e) {
+            string[] args = Environment.GetCommandLineArgs();
+            string file;
+            if (args.Length < 2) {
+                var dialog = new OpenFileDialog() {
+                    AddExtension = true,
+                    AutoUpgradeEnabled = true,
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    FileName = "",
+                    InitialDirectory = Environment.CurrentDirectory,
+                    Multiselect = false,
+                    Title = "Choose a file to delete"
+                };
+                dialog.ShowDialog();
+                file = dialog.FileName;
+                if (string.IsNullOrEmpty(file)) Environment.Exit(1223);
+            } else {
+                file = args[1];
+            }
+            // detect admin rights
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent()) {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                if (!principal.IsInRole(WindowsBuiltInRole.Administrator)) {
+                    MessageBox.Show("The application must be run under administrative privileges.", default, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(5);
+                }
+            }
+            // detect if user want to add registery key
+            if (file == "/reg") {
+                try {
+                    using (var rootKey = Registry.ClassesRoot.CreateSubKey(@"*\shell\DeleteOnReboot")) {
+                        rootKey.SetValue("", "Delete on &Reboot");
+                        rootKey.SetValue("HasLUAShield", "");
+                        using (var commandKey = rootKey.CreateSubKey("command")) {
+                            commandKey.SetValue("", $"\"{Assembly.GetExecutingAssembly().Location}\" \"%1\"");
+                        }
+                    }
+                } catch (Exception ex) {
+                    MessageBox.Show($"Unable to add to context menu: {ex.Message}", default, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(ex.HResult);
+                }
+                MessageBox.Show($"Successfully added to context menu.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Environment.Exit(0);
+            }
+            // detect file existence
+            if (!File.Exists(file)) {
+                MessageBox.Show("Specified file does not exist.", default, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(3);
+            }
+            // delete file
+            var result = MoveFileExW(file, null, MOVEFILE_DELAY_UNTIL_REBOOT);
+            if (!result) {
+                var exc = new Win32Exception(Marshal.GetLastWin32Error());
+                MessageBox.Show($"Failed to delete \"{file}\" (0x{exc.HResult.ToString("X")}): {exc.Message}", default, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(exc.ErrorCode);
+            } else {
+                MessageBox.Show($"Successfully scheduled deletion of \"{file}\" on next reboot.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Environment.Exit(0);
+            }
+        }
+
+        const uint MOVEFILE_DELAY_UNTIL_REBOOT = 0x4;
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern bool MoveFileExW(string lpExistingFileName, string lpNewFileName, uint dwFlags);
+    }
+}
